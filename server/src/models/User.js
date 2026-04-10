b4d7f9aae5b6d69ba8427063fdd58b79e7366ea2
sumon2003 <sumonkhanbd2003@gmail.com>
2026-04-10 07:37:13 +0600

    Update user role schema to use enum for better validation

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,24 +5,34 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }, 
-  role: { type: String, default: "user" },
+  
+  role: { 
+    type: String, 
+    enum: ["Customer", "Admin"], 
+    default: "Customer" 
+  },
+  
   isAdmin: { 
     type: Boolean, 
     default: false, 
     required: true 
   },
+
+  isBlocked: { 
+    type: Boolean, 
+    default: false 
+  },
+
   isEmailVerified: { type: Boolean, default: false },
   emailVerifyTokenHash: String,
   emailVerifyTokenExpires: Date,
 }, { timestamps: true });
-
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
